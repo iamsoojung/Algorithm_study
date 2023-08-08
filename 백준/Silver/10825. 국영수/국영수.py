8e71ef2359f5eed672c2189165734e25ec5b1d41
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2023-08-08 13:45:29 +0900

    [Silver IV] Title: 국영수, Time: 548 ms, Memory: 64084 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+score_list = []
+
+for i in range(N):
+    [name, kor, eng, math] = map(str, sys.stdin.readline().split())
+    score_list.append([name, int(kor), int(eng), int(math)])
+
+score_list = sorted(score_list, key=lambda x : (-x[1], x[2], -x[3], x[0]))
+
+for score in score_list:
+    print(score[0])
