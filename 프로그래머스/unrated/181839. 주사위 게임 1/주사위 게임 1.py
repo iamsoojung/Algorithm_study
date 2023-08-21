0371c0600f224275941d3e691c220019dd45701e
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2023-08-21 20:28:03 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
@@ -0,0 +1,8 @@
+def solution(a, b):
+    # 모두 짝수
+    if (a % 2 == 0 and b % 2 == 0):
+        return abs(a-b)
+    # 모두 홀수
+    if (a % 2 == 1 and b % 2 == 1):
+        return (a*a) + (b*b)
+    return 2 * (a+b)
