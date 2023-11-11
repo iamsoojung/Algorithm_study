65aaabe616010eb7eecd790b19de250e1ed1385a
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2023-11-11 16:30:44 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,17 +1,3 @@
 def solution(survey, choices):
     answer = ''
-    mbti = {"R": 0, "T": 0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0}
-    
-    for i, (s, c) in enumerate(zip(survey, choices)):
-        diff = c - 4
-        if diff < 0:
-            mbti[s[0]] += abs(diff)
-        elif diff > 0:
-            mbti[s[1]] += diff
-    
-    answer += "T" if mbti["R"] < mbti["T"] else "R"
-    answer += "F" if mbti["C"] < mbti["F"] else "C"
-    answer += "M" if mbti["J"] < mbti["M"] else "J"
-    answer += "N" if mbti["A"] < mbti["N"] else "A"
-    
     return answer
