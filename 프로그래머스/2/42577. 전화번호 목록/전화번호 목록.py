fc4c64f6329b9ec1990e6c33d63f3473ff645fea
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2023-11-23 16:38:14 +0900

    [level 2] Title: 전화번호 목록, Time: 485.78 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,15 +1,16 @@
 def solution(phone_book):
-    # 1. Hash map을 만든다
-    hash_map = {}
-    for phone_number in phone_book:
-        hash_map[phone_number] = 1
+    answer = True
+    dict = {}
     
-    # 2. 접두어가 Hash map에 존재하는지 찾는다
-    for phone_number in phone_book:
-        jubdoo = ""
-        for number in phone_number:
-            jubdoo += number
-            # 3. 접두어를 찾아야 한다 (기존 번호와 같은 경우 제외)
-            if jubdoo in hash_map and jubdoo != phone_number:
-                return False
-    return True
+    for p in phone_book:
+        dict[p] = 0
+    
+    for p in phone_book:
+        check = ''
+        for i in p:
+            check += i
+            if (check in dict) and (check != p):
+                answer = False
+                break
+    
+    return answer
