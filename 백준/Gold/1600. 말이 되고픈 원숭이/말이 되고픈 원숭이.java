b6f636d98161ed625d21a6ad47e5ba9c27b58a61
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2024-03-04 08:48:13 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 544 ms, Memory: 61576 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,102 +1,85 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int K, W, H, answer = 0;
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	static int[] hdy= {1,-1, 2,-2, 2,-2, 1,-1};
+	static int[] hdx= {2, 2, 1, 1,-1,-1,-2,-2};
+	static int[] dy= {1,-1, 0, 0};
+	static int[] dx= {0, 0,-1, 1};
 	static int[][] map;
-	static boolean[][][] visit;
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	static int[] hx = {-1, -2, -2, -1, 1, 2, 2, 1};
-	static int[] hy = {-2, -1, 1, 2, 2, 1, -1, -2};
-
-	public static void main(String[] args) throws IOException {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		K = Integer.parseInt(br.readLine());	// 움직일 수 있는 횟수
-		
-		st = new StringTokenizer(br.readLine());
-		W = Integer.parseInt(st.nextToken());	// 가로
-		H = Integer.parseInt(st.nextToken());	// 세로
-		
-		map = new int[H][W];	// 0 : 아무것도 없는 평지, 1 : 장애물
-		for (int i=0; i<H; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j=0; j<W; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
+	static int K, W, H;
+	
+	static class Point{
+		int i;
+		int j;
+		int horseMove;
+		int move;
+		public Point(int i, int j, int horseMove, int move) {
+			super();
+			this.i = i;
+			this.j = j;
+			this.horseMove = horseMove;
+			this.move = move;
 		}
-		
-		visit = new boolean[H][W][K+1];
-		bfs(0, 0);
-		
-		System.out.println(answer);
 	}
 	
-	static void bfs(int x, int y) {
-		Queue<Node> q = new ArrayDeque<>();
-		q.offer(new Node(x, y, 0, 0));
-		visit[x][y][0] = true;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		K=Integer.parseInt(br.readLine());//K번만 말처럼 움직일 수 있다
+		String[] in1=br.readLine().split(" ");
+		W=Integer.parseInt(in1[0]);
+		H=Integer.parseInt(in1[1]);
+		map=new int[H][W];
+		
+		for(int i=0;i<H;i++) {
+			String[] in2=br.readLine().split(" ");
+			for(int j=0;j<W;j++) {
+				map[i][j]=Integer.parseInt(in2[j]);
+			}
+		}//end for
+		
+		bw.write(Integer.toString(bfs()));
+		bw.flush();
+		
+	}//end main
+	
+	static int bfs() {
+		Queue<Point> q=new ArrayDeque<Point>();
+		boolean[][][]visited=new boolean[H][W][K+1];
+		q.offer(new Point(0, 0, K,0));
+		visited[0][0][K]=true;
 		
 		while(!q.isEmpty()) {
-			Node cur = q.poll();
-			int cx = cur.x;
-			int cy = cur.y;
-			
-			// 도착한 경우
-			if (cx == H-1 && cy == W-1) {
-				answer = cur.dist;
-				return;
-			}
-			
-			// k 이하로 말처럼 움직였다면
-			//		말 처럼 움직이기
-			// 상하좌우 움직이기
-			if (cur.horseCnt < K) {
-				for (int d=0; d<8; d++) {
-					int nx = cx + hx[d];
-					int ny = cy + hy[d];
-					
-					if (0<=nx && nx<H && 0<=ny && ny<W && !visit[nx][ny][cur.horseCnt+1]
-							&& map[nx][ny] == 0) {
-						visit[nx][ny][cur.horseCnt+1] = true;
-						q.offer(new Node(nx, ny, cur.dist+1, cur.horseCnt+1));
-					}
+			for(int i=0;i<q.size();i++) {
+				Point curr=q.poll();
+				if(curr.i==H-1&&curr.j==W-1) return curr.move;
+				for(int d=0;d<4;d++) {
+					int ny=curr.i+dy[d];
+					int nx=curr.j+dx[d];
+					if(ny>=0&&nx>=0&&ny<H&&nx<W&&map[ny][nx]!=1&&!visited[ny][nx][curr.horseMove]) {
+						visited[ny][nx][curr.horseMove]=true;
+						q.offer(new Point(ny, nx, curr.horseMove, curr.move+1));
+					}//end if
+				}//end for dy dx
+				if(curr.horseMove>0) {
+					for(int d=0;d<8;d++) {
+						int ny=curr.i+hdy[d];
+						int nx=curr.j+hdx[d];
+						if(ny>=0&&nx>=0&&ny<H&&nx<W&&map[ny][nx]!=1&&!visited[ny][nx][curr.horseMove-1]) {
+							visited[ny][nx][curr.horseMove-1]=true;
+							q.offer(new Point(ny, nx, curr.horseMove-1, curr.move+1));
+						}//end if
+					}//end for dy dx
 				}
-			}
-			for (int d=0; d<4; d++) {
-				int nx = cx + dx[d];
-				int ny = cy + dy[d];
-				
-				if (0<=nx && nx<H && 0<=ny && ny<W && !visit[nx][ny][cur.horseCnt]
-						&& map[nx][ny] == 0) {
-					visit[nx][ny][cur.horseCnt] = true;
-					q.offer(new Node(nx, ny, cur.dist+1, cur.horseCnt));
-				}
-			}
-		}
-		answer = -1;
-		return;
-	}
-	 
-	static class Node {
-		int x;
-		int y;
-		int dist;
-		int horseCnt;
-		
-		public Node(int x, int y, int dist, int horseCnt) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.dist = dist;
-			this.horseCnt = horseCnt;
-		}
-	}
+			}//end for
+		}//end while
+		return -1;
+	}//end bfs
 }
