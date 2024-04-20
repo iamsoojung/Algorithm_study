9efa6cf5e10716f9bf97ad5c298210c6bf944a6f
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2024-04-21 00:01:05 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 128 ms, Memory: 27,328 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,63 +1,76 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int p[];
-	static int rank[];
-	static int answer;
-	static StringBuilder sb = new StringBuilder();
 	
+	static int N, M, answer;
+	static ArrayList<Integer>[] arr;
+	static boolean[] visit;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
 		
+		int T = Integer.parseInt(br.readLine());
 		for (int testCase=1; testCase<=T; testCase++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());	// 마을에 사는 사람 수
-			int M = Integer.parseInt(st.nextToken());	// 서로 알고 있는 사람의 관계 ㅅ
+			st = new StringTokenizer(br.readLine());
 			
-			p = new int[N+1];
-			rank = new int[N+1];
+			N = Integer.parseInt(st.nextToken());	// 사람 수
+			M = Integer.parseInt(st.nextToken());	// 사람 간 관계 수
+			
+			arr = new ArrayList[N+1];
 			for (int i=1; i<=N; i++) {
-				p[i] = i;
+				arr[i] = new ArrayList<>();
 			}
 			
+			int a, b;
 			for (int i=0; i<M; i++) {
 				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				union(a, b);
+				a = Integer.parseInt(st.nextToken());
+				b = Integer.parseInt(st.nextToken());
+				arr[a].add(b);
+				arr[b].add(a);
 			}
-
+			
+//			for (int i=1; i<=N; i++) {
+//				System.out.println(arr[i].toString());
+//			}
+			
+			visit = new boolean[N+1];
 			answer = 0;
 			for (int i=1; i<=N; i++) {
-				if (p[i] == i) {
+				if (!visit[i]) {
 					answer++;
+					bfs(i);
 				}
 			}
+			
 			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
 		}
-		System.out.print(sb);
-	}
-
-	static void union(int x, int y) {
-		x = find(x);
-		y = find(y);
-
-		if (rank[x] < rank[y]) {
-			p[x] = y;
-			return;
-		}
-		if (rank[x] == rank[y])	rank[y]++;		
-		p[y] = x;
+		System.out.print(sb.toString());
 	}
 	
-	static int find(int x) {
-		if (p[x] == x) {
-			return x;
+	static void bfs(int idx) {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.add(idx);
+		visit[idx] = true;
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			visit[cur] = true;
+			
+			for (int i=0; i<arr[cur].size(); i++) {
+				int next = arr[cur].get(i);
+				if (!visit[next]) {					
+					q.add(next);
+				}
+			}
 		}
-		return p[x] = find(p[x]);
 	}
 }
