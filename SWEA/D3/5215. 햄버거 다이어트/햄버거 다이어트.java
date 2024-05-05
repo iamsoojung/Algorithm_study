64d7af12f96738b4ce49d40ba6a9364618b6bd44
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2024-05-05 11:55:12 +0900

    [D3] Title: 햄버거 다이어트, Time: 184 ms, Memory: 27,024 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -32,6 +32,7 @@ public class Solution {
 			
 			select = new boolean[N];
 			answer = Integer.MIN_VALUE;
+			
 			// 제한 칼로리 이하, 최대 점수
 			subset(0, 0, 0);
 			
@@ -40,27 +41,17 @@ public class Solution {
 		System.out.println(sb.toString());
 	}
 	
-	static void subset(int depth, int idx, int cal) {
-		if (cal > L)	return;		// 제한 칼로리 넘은 경우 바로 종료
+	static void subset(int cnt, int calSum, int scoreSum) {
+		if (calSum > L)	return;		// 제한 칼로리 넘은 경우 바로 종료
 		
-		if (depth == N) {
-			answer = Math.max(answer, getScore());
+		if (cnt == N) {
+			answer = Math.max(answer, scoreSum);
 			return;
 		}
 		
-		select[idx] = true;
-		subset(depth+1, idx+1, cal + topping[idx][1]);
-		select[idx] = false;
-		subset(depth+1, idx+1, cal);
-	}
-	
-	static int getScore() {
-		int cnt = 0;
-		for (int i=0; i<N; i++) {
-			if (select[i]) {
-				cnt += topping[i][0];
-			}
-		}
-		return cnt;
+		select[cnt] = true;
+		subset(cnt+1, calSum + topping[cnt][1], scoreSum + topping[cnt][0]);
+		select[cnt] = false;
+		subset(cnt+1, calSum, scoreSum);
 	}
 }
