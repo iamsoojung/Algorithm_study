e8397db17830c59c47f99970afc594f8e1df6723
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2024-05-07 16:34:55 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 180 ms, Memory: 33,532 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,76 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
 	
 	static int N, M, answer;
-	static ArrayList<Integer>[] arr;
+	static int[][] graph;
 	static boolean[] visit;
-
+	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		StringBuilder sb = new StringBuilder();
 		
 		int T = Integer.parseInt(br.readLine());
 		for (int testCase=1; testCase<=T; testCase++) {
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());	// 마을에 사는 사람 수
+			M = Integer.parseInt(st.nextToken());	// 서로 알고 있는 사람의 관계 ㅅ
+
+			graph = new int[N+1][N+1];
+			visit = new boolean[N+1];
 			
-			N = Integer.parseInt(st.nextToken());	// 사람 수
-			M = Integer.parseInt(st.nextToken());	// 사람 간 관계 수
-			
-			arr = new ArrayList[N+1];
-			for (int i=1; i<=N; i++) {
-				arr[i] = new ArrayList<>();
-			}
-			
-			int a, b;
 			for (int i=0; i<M; i++) {
 				st = new StringTokenizer(br.readLine());
-				a = Integer.parseInt(st.nextToken());
-				b = Integer.parseInt(st.nextToken());
-				arr[a].add(b);
-				arr[b].add(a);
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				graph[a][b] = 1;
+				graph[b][a] = 1;
 			}
-			
-//			for (int i=1; i<=N; i++) {
-//				System.out.println(arr[i].toString());
-//			}
-			
-			visit = new boolean[N+1];
+
 			answer = 0;
 			for (int i=1; i<=N; i++) {
 				if (!visit[i]) {
+					dfs(i);
 					answer++;
-					bfs(i);
 				}
 			}
 			
 			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
 		}
-		System.out.print(sb.toString());
+		System.out.print(sb);
 	}
-	
-	static void bfs(int idx) {
-		Queue<Integer> q = new ArrayDeque<>();
-		q.add(idx);
-		visit[idx] = true;
-		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			visit[cur] = true;
-			
-			for (int i=0; i<arr[cur].size(); i++) {
-				int next = arr[cur].get(i);
-				if (!visit[next]) {					
-					q.add(next);
-				}
+
+	// 연결되어있는지 확인
+	static void dfs(int start) {
+		visit[start] = true;
+		for (int i=1; i<=N; i++) {
+			if (graph[start][i] == 1 && !visit[i]) {	// 연결됨 && 방문X
+				dfs(i);
 			}
 		}
+		return;
 	}
 }
