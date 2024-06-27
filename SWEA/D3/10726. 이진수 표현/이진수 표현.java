0a63a565d1c52b3b989f0243ac64ddc1d0499028
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2024-06-27 11:47:51 +0900

    [D3] Title: 이진수 표현, Time: 177 ms, Memory: 28,572 KB -BaekjoonHub

---
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int testCase=1; testCase<=T; testCase++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			int lastNBit = (1<<N) - 1;	// 마지막 N 비트 모두 1인 수
+			if (lastNBit == (lastNBit & M)) {
+				sb.append("#").append(testCase).append(" ON\n");
+			} else {
+				sb.append("#").append(testCase).append(" OFF\n");
+			}
+		}
+		System.out.println(sb.toString());
+	}
+}
