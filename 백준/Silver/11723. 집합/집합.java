0e7de7b6df2c05426e79fd268638a9f9b0833fa0
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2024-06-29 21:24:09 +0900

    [Silver V] Title: 집합, Time: 964 ms, Memory: 325268 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,67 +1,58 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
-public class Main {
-	
-	public static void main (String[] args) throws IOException {
+class Main {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
+		
 		int M = Integer.parseInt(br.readLine());
-		int bitSet = 0;
+		int bit = 0;
 		
 		for (int i=0; i<M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			String op = st.nextToken();
+			int x = 0;
 			
-			if (st.countTokens() == 0) {
-				if (op.equals("all")) {
-					for (int j=1; j<=20; j++) {
-						bitSet |= (1<<j);	// j번째가 1이 되도록
+			switch(op) {
+				case "add":
+					x = Integer.parseInt(st.nextToken());
+					bit |= (1<<x);
+					break;
+				case "remove":
+					x = Integer.parseInt(st.nextToken());
+					if ((bit & (1<<x)) != 0) {	// 존재한다면
+						bit ^= (1<<x);
 					}
-					continue;
-				}
-				if (op.equals("empty")) {
-					bitSet = 0;
-					continue;
-				}
-			}
-			else {
-				int x = Integer.parseInt(st.nextToken());
-				switch(op) {
-					case "add":
-						bitSet = bitSet | (1<<x);	// x자리를 1로 (OR)
-						break;
-					case "remove":
-						if ((bitSet & (1<<x)) != 0) {	// 있다면
-							bitSet = bitSet ^ (1<<x);	// x자리를 0으로 (XOR)
-						};
-						break;
-					case "check":
-						if ((bitSet & (1<<x)) != 0) {
-							sb.append(1 + "\n");
-						} else {
-							sb.append(0 + "\n");
-						}
-						break;
-					case "toggle":
-						if ((bitSet & (1<<x)) != 0) {	// 있다면
-							bitSet = bitSet ^ (1<<x);	// 제거 (XOR)
-						} else {
-							bitSet = bitSet | (1<<x);	// 추가 (OR)
-						}
-						break;
-				}
+					break;
+				case "check":
+					x = Integer.parseInt(st.nextToken());
+					if ((bit & (1<<x)) != 0) {
+						sb.append("1\n");
+					} else {
+						sb.append("0\n");
+					}
+					break;
+				case "toggle":
+					x = Integer.parseInt(st.nextToken());
+					if ((bit & (1<<x)) != 0) {
+						bit ^= (1<<x);
+					} else {
+						bit |= (1<<x);
+					}
+					break;
+				case "all":
+					for (int j=1; j<=20; j++) {
+						bit |= (1<<j);
+					}
+					break;
+				case "empty":
+					bit = 0;
+					break;
 			}
 		}
-		bw.write(sb.toString());
-		bw.close();
-		br.close();
+		System.out.println(sb.toString());
 	}
 }
