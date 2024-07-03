3b6e0cb5666d5b860f189a1d32fc532588205a3e
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2024-07-03 10:51:00 +0900

    [Silver V] Title: 덩치, Time: 76 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,26 +1,33 @@
-import java.util.Scanner;
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
 		int[][] spec = new int[N][2];
 		
 		for (int i=0; i<N; i++) {
-		    spec[i][0] = sc.nextInt();
-		    spec[i][1] = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
+			spec[i][0] = Integer.parseInt(st.nextToken());
+			spec[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
 		for (int i=0; i<N; i++) {
-		    int rank = 1;
-		    
-		    for (int j=0; j<N; j++) {
-		        if (i==j)   continue;
-		        if (spec[i][0] < spec[j][0] && spec[i][1] < spec[j][1]) {
-		            rank++;
-		        }
-		    }
-    		System.out.print(rank + " ");
+			int rank = 1;
+			for (int j=0; j<N; j++) {
+				if (i==j)	continue;
+				if (spec[i][0] < spec[j][0] && spec[i][1] < spec[j][1]) {
+					rank++;
+				}
+			}
+			sb.append(rank).append(" ");
 		}
+		System.out.println(sb.toString());
 	}
 }
