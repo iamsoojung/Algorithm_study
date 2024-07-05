5625886ebb9264f44e6fd5e26f2d47d4c87ddd44
Soojung Kim <65272297+iamsoojung@users.noreply.github.com>
2024-07-05 16:11:36 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 196 ms, Memory: 25568 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,31 +10,17 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		
-		HashMap<String, Integer> map = new HashMap<>();
+		Set<String> set = new HashSet<>();
 		
 		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());	// 플레이 신청 횟수 
-		char game = st.nextToken().charAt(0);		// 플레이할 게임 종류
+		String game = st.nextToken();		// 플레이할 게임 종류
+		int gameN = game.equals("Y") ? 1 : game.equals("F") ? 2 : 3;
 		
 		for (int i=0; i<N; i++) {
-			String s = br.readLine();
-			if (map.containsKey(s)) {
-				map.replace(s, map.get(s) + 1);
-			} else {
-				map.put(s, 1);
-			}
+			set.add(br.readLine());
 		}
 		
-		switch(game) {
-		case 'Y':
-			System.out.println(map.size());
-			break;
-		case 'F':
-			System.out.println(map.size()/2);
-			break;
-		case 'O':
-			System.out.println(map.size()/3);
-			break;
-		}
+		System.out.print(set.size() / gameN);
 	}
 }
