11274de322e3b6181551aa787a23b3c1ec9a8793
Soojung Kim <crystar2402@naver.com>
2024-11-01 13:43:37 +0900

    [level 2] Title: 타겟 넘버, Time: 7.95 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,21 @@
 class Solution {
-    int answer = 0;
+    static int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, target, 0);
+        
+        dfs(0, numbers, target, 0);        
         return answer;
     }
-
-    public void dfs(int[] numbers, int depth, int target, int sum) {
+    
+    static void dfs(int depth, int[] numbers, int target, int sum) {
         if (depth == numbers.length) {
-            if (target == sum)  answer++;
-        } else {
-            dfs(numbers, depth+1, target, sum + numbers[depth]);
-            dfs(numbers, depth+1, target, sum - numbers[depth]);
+            if (target == sum) {
+                answer++;
+            }
+            return;
         }
+        
+        dfs(depth+1, numbers, target, sum + numbers[depth]);
+        dfs(depth+1, numbers, target, sum - numbers[depth]);
     }
 }
