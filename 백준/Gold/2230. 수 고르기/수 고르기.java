762625857c582229315842abfb0a01a9eeeb0e46
Soojung Kim <crystar2402@naver.com>
2025-03-02 18:00:31 +0900

    [Gold V] Title: 수 고르기, Time: 272 ms, Memory: 27916 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    
+	    int N = Integer.parseInt(st.nextToken());
+	    int M = Integer.parseInt(st.nextToken());
+	    
+	    int[] arr = new int[N];
+	    for (int i=0; i<N; i++) {
+	        arr[i] = Integer.parseInt(br.readLine());
+	    }
+	    Arrays.sort(arr);
+	    
+	    int i = 0, j = 0;
+        int ans = Integer.MAX_VALUE;
+
+        while (i < N) {
+            if (arr[i] - arr[j] < M) {
+                i++;
+                continue;
+            }
+ 
+            if (arr[i] - arr[j] == M) {
+                ans = M;
+                break;
+            }
+ 
+            ans = Math.min(ans, arr[i] - arr[j]);
+            j++;
+        }
+	    
+	    System.out.println(ans);
+	}
+}
