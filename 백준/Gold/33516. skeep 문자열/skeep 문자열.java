4eabb2a0f4ecb9072853ffaf520c6f0968e7b366
Soojung Kim <crystar2402@naver.com>
2025-03-09 22:44:15 +0900

    [Gold III] Title: skeep 문자열, Time: 256 ms, Memory: 25316 KB -BaekjoonHub

--- 백준/Gold/33516. skeep 문자열/skeep 문자열.java
+++ 백준/Gold/33516. skeep 문자열/skeep 문자열.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 public class Main {
 	
@@ -10,7 +10,7 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		String s = br.readLine();
 		
-		Stack<Integer> stack = new Stack<>();
+		ArrayDeque<Integer> stack = new ArrayDeque<>();
 		int answer = 0;
 		
 		for (int i=0; i<N; i++) {
