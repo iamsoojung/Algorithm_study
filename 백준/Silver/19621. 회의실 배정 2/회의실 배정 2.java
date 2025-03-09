96ea7d69838d3ca66d767e349457ec98d823afd5
Soojung Kim <crystar2402@naver.com>
2025-03-09 23:11:56 +0900

    [Silver II] Title: 회의실 배정 2, Time: 68 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.java
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.java
@@ -6,35 +6,38 @@ import java.util.StringTokenizer;
 public class Main {
 	
 	static int N, answer = Integer.MIN_VALUE;	// 최대 인원
-	static int[][] meetings;
+	static int[] schedule, dp;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		
 		N = Integer.parseInt(br.readLine());
-		meetings = new int[N][3];
+
+		schedule = new int[N+1];
+		dp = new int[N+1];
 		
-		for (int i=0; i<N; i++) {
+		for (int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
-			meetings[i][0] = Integer.parseInt(st.nextToken());
-			meetings[i][1] = Integer.parseInt(st.nextToken());
-			meetings[i][2] = Integer.parseInt(st.nextToken());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int cnt = Integer.parseInt(st.nextToken());
+			
+			// schedule 배열에 인원수 저장
+			schedule[i] = cnt;
 		}
 		
-		dfs(0, 0);
-		System.out.print(answer);
-	}
-	
-	static void dfs(int sum, int curStart) {
-		if (answer < sum) {
-			answer = sum;
+		dp[1] = schedule[1];
+		
+		if (N >= 2) {	// 회의가 2개 이상인 경우에만
+			dp[2] = Math.max(schedule[1], schedule[2]);	// 1번 회의, 2번 회의 중 인원수 많은거 저장 (겹치기 때문)
 		}
 		
-		for (int i=0; i<N; i++) {
-			if (curStart <= meetings[i][0]) {
-				dfs(sum + meetings[i][2], meetings[i][1]);
-			}
+		for (int i=3; i<=N; i++) {
+			// i-1까지의 최대 인원수 vs (i-2까지의 최대 인원수 + 현재 인원수)
+			dp[i] = Math.max(dp[i-1], dp[i-2] + schedule[i]);
 		}
+		
+		System.out.print(dp[N]);
 	}
 }
